fdd90cf358593ee2c03acb179ef588c59a5f03b1
roua <roua.LAADOUZ@b2m-it.com>
2022-05-02 04:29:48 +0100

    ajout du mapper

---
+++ formation-api/src/main/java/com/projet/formation/Dto/UserDto.java
@@ -0,0 +1,111 @@
+package com.projet.formation.dto;
+
+import com.projet.formation.models.Role;
+import org.bongiorno.dto.support.AbstractDto;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDto extends AbstractDto {
+    Long id;
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+    String email;
+    private Date creationDate;
+    private Date modificationDate;
+    Boolean codePin;
+    Boolean activated;
+
+    private Set<Role> authorities = new HashSet<>();
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Role> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Role> authorities) {
+        this.authorities = authorities;
+    }
+
+    public Boolean getActivated() {
+        return activated;
+    }
+
+    public void setActivated(Boolean activated) {
+        this.activated = activated;
+    }
+
+    public Boolean isCodePin() {
+        return codePin;
+    }
+
+    public void setCodePin(Boolean codePin) {
+        this.codePin = codePin;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getModificationDate() {
+        return modificationDate;
+    }
+
+    public void setModificationDate(Date modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+}
--- formation-api/src/main/java/com/projet/formation/controllers/UserController.java
+++ formation-api/src/main/java/com/projet/formation/controllers/UserController.java
@@ -1,5 +1,9 @@
 package com.projet.formation.controllers;
 
+import com.projet.formation.dto.AjoutResponse;
+import com.projet.formation.dto.LoginDto;
+import com.projet.formation.dto.UserDto;
+import com.projet.formation.mapper.ObjectMapperUtils;
 import com.projet.formation.models.User;
 import com.projet.formation.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +12,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Date;
 import java.util.List;
+import org.modelmapper.ModelMapper;
 
 
 @RestController
@@ -17,11 +24,58 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    ModelMapper modelMapper = new ModelMapper();
 
 
     @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<User>> getAllUsers(@RequestParam(name = "login", required = true) String login) {
         return new ResponseEntity<>(userService.getAllUsers(login), HttpStatus.OK);
+    }
+    @PostMapping(value = "/addUser")
+    public ResponseEntity<UserDto> addOrUpdateUser(@Valid @RequestBody UserDto userDto) {
+        if (userDto.getId() == null) {
+            userDto.setCreationDate(new Date());
+        } else {
+            userDto.setModificationDate(new Date());
+        }
+        User user = ObjectMapperUtils.map(userDto,User.class);
+        UserDto userDto1 = ObjectMapperUtils.map(userService.addOrUpdateUser(user),UserDto.class);
+        return new ResponseEntity<>(userDto1, HttpStatus.OK);
+    }
+    @GetMapping("/getUserByLogin")
+    public ResponseEntity<UserDto> getUserByLogin(@RequestParam(name = "login", required = true) String login) {
+        UserDto userDTO = modelMapper.map(userService.getAllUsers(login), UserDto.class);
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
+
+//    @GetMapping("/getUserById")
+//    public ResponseEntity<UserDto> getUserById(@RequestParam(name = "id", required = true) Long id) {
+//        UserDto userDTO = modelMapper.map(userRepository.getOne(id), UserDto.class);
+//        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+//    }
+
+    @PostMapping("/deleteUser")
+    public ResponseEntity deleteUser(@RequestBody long id) {
+        AjoutResponse ajoutResponse = new AjoutResponse();
+        try{
+            ajoutResponse = userService.deleteUser( id);
+
+        }catch (Exception e){
+            ajoutResponse.setMessage(e.getMessage());
+        }
+        return new ResponseEntity(ajoutResponse,HttpStatus.OK);
+    }
+
+    @PostMapping("/verifyPassword")
+    public ResponseEntity<Boolean> verifyPassword(@RequestBody LoginDto loginDto){
+        Boolean updatePassword = userService.verifyPassword(loginDto);
+        return new ResponseEntity(updatePassword,HttpStatus.OK);
+    }
+
+    @PostMapping("/checkPassword")
+    public ResponseEntity<Boolean> checkPassword(@RequestBody com.b2m.sbsresto.dto.CheckPwd checkPwd){
+        Boolean check = userService.checkPassword( checkPwd.getPwdUser(), checkPwd.getPasswordToCheck());
+        return new ResponseEntity(check,HttpStatus.OK);
     }
 
 }

298577b15e0a028d74d8c61ccec38a5be2556357
roua <roua.LAADOUZ@b2m-it.com>
2022-05-14 01:39:34 +0100

    parametrePage

--- formation-ui/src/app/parametre/parametre.component.ts
+++ formation-ui/src/app/parametre/parametre.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {LocalStorageService} from "ngx-webstorage";
+import {Role} from "../model/Role";
 
 @Component({
   selector: 'app-parametre',
@@ -7,11 +9,16 @@ import {Title} from "@angular/platform-browser";
   styleUrls: ['./parametre.component.css']
 })
 export class ParametreComponent implements OnInit {
-
-  constructor(private title: Title) { }
+  username!:string;
+  roles!:Role[];
+  role!:string;
+  constructor(private title: Title,private localStorageService: LocalStorageService) { }
 
   ngOnInit(): void {
     this.title.setTitle('Paramétre')
+    this.username=this.localStorageService.retrieve("login");
+    this.roles=this.localStorageService.retrieve('authorities');
+    this.role=this.roles[0].name;
   }
 
 }
